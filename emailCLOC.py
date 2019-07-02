74eaa23bcf25d6f50ee2f1f7f827b6fda510805d
classic89 <retrorock8821@gmail.com>
2019-07-02 12:31:31 -0500

    Update emailCLOC.py

--- emailCLOC.py
+++ emailCLOC.py
@@ -3,45 +3,39 @@ from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from email.mime.base import MIMEBase
 from email import encoders
-from pip._internal import main
 from datetime import datetime
 from secrets import myDefaultPassword
 
 class emailCLOC:
-
-    # Get repository name from URL
-    def getRepository(self):
-        return(self.repoUrl[self.repoUrl.rfind('/')+1:self.repoUrl.rfind('.')])
-
-    def validateEmail(self, email):
-        errorMessage = email
-        if not re.match(r"[^@]+@[^@]+\.[^@]+", email):
-            errorMessage = errorMessage+"Email format is invalid\n"
-
-    def validateURL(self, url):
-        errorMessage = url
-        if not re.match(r"((git|ssh|http(s)?)|(git@[\w\.]+))(:(//)?)([\w\.@\:/\-~]+)(\.git)(\/)?", url) and (not len(url)==0):
-            errorMessage = errorMessage +"Git repository is invalid.\n"
+    def __init__(self, myEmail, myPassword, yourEmail, repoURL, repoName, branch, outputFile, clocfile):
+        self.myEmail= myEmail
+        self.myPassword = myPassword
+        self.yourEmail = yourEmail
+        self.repoUrl = repoURL
+        self.repoName = repoName
+        self.branch = branch
+        self.outputFile = outputFile
+        self.clocfile = clocfile
 
     def gitinit(self):
-        repo = self.getRepository()
+        name = self.repoName
         currentDirectory = "./"
-        if not os.path.exists(currentDirectory+repo):
-            print ('Cloning ' + repo + '...')
+        if not os.path.exists(currentDirectory+name):
+            print ('Cloning ' + name + '...')
             git.Git(currentDirectory).clone(self.repoUrl)
-        gitrepo = git.Repo(repo)
-        gcmd = git.cmd.Git(repo)
+        gitrepo = git.Repo(name)
+        gcmd = git.cmd.Git(name)
         gcmd.pull()
-        print ('Checking out'+ repo + '...')
+        print ('Checking out'+ name + '...')
         gitrepo.git.checkout("-f", self.branch)
 
-        print ('Running CLOC report for ' + repo + '...')
-        proc = subprocess.Popen([currentDirectory+self.clocfile,currentDirectory+repo, "--csv","--out", self.outputFile , "--quiet"], stdout=subprocess.PIPE)
+        print ('Running CLOC report for ' + name + '...')
+        proc = subprocess.Popen([currentDirectory+self.clocfile,currentDirectory+name, "--csv","--out", self.outputFile , "--quiet"], stdout=subprocess.PIPE)
         proc.stdout.read()
 
     def sendEmail(self):
         msg = MIMEMultipart()
-        msg['Subject'] = 'The CLOC Report of the %s epository' % self.getRepository()
+        msg['Subject'] = 'The CLOC Report of the %s epository' % self.repoName
         msg['From'] = self.myEmail
         msg['To'] = self.yourEmail
         part = MIMEBase('application', "octet-stream")
@@ -57,66 +51,76 @@ class emailCLOC:
             server.login(self.myEmail, self.myPassword)
             server.sendmail(self.myEmail, self.yourEmail, msg.as_string())
 
-    # def speedInput(input):
-    #     #test input
-    #     me = "retrorock8821@gmail.com"
-    #     you = "mw103110@gmail.com"
-    #     defaultRepoURL = "https://github.com/classic89/Forbidden-Island.git"
-    #     defaultBranch = "master"
+    def validateInput(self):
+        validateEmail(self.myEmail)
+        print("From : "+self.myEmail)
+        validateEmail(self.yourEmail)
+        print("To : "+self.yourEmail)
+        validateURL(self.repoUrl)
+        print("Repository : "+self.repoUrl)
+        print("Branch : "+self.branch)
+        print("Attachment : "+self.outputFile)
 
-    #     if (input is emailCLOC.myEmail):
-    #         emailCLOC.myEmail = me
-    #     if (input is emailCLOC.myPassword):
-    #         emailCLOC.myPassword = myDefaultPassword.decode()
-    #     if (input is emailCLOC.yourEmail):
-    #         emailCLOC.yourEmail = you
-    #     if (input is emailCLOC.repoUrl):
-    #         emailCLOC.repoUrl = defaultRepoURL
-    #     if (input is emailCLOC.branch):
-    #         emailCLOC.branch = defaultBranch
-    #     emailCLOC.validateInput(myEmail, yourEmail, myPassword, repoURL, branch, outputFile)
+#helper methods
+# class helperMethods: 
+#     def __init__():
+#         self.getRepository()
+#         self.validateEmail()
+#         self.
 
-    def validateInput(self, myEmail, yourEmail, myPassword, repoURL, branch, outputFile):
-        self.validateEmail(myEmail)
-        print("From :"+myEmail)
-        self.validateEmail(yourEmail)
-        print("To: "+yourEmail)
-        self.validateURL(repoURL)
-        print("Repository : "+repoURL)
-        print("Branch : "+branch)
-        print("Attachment: "+outputFile)
-        
+def getRepository(repoUrl):
+        return(repoUrl[repoUrl.rfind('/')+1:repoUrl.rfind('.')])
 
-    def __init__(self, myEmail, yourEmail, myPassword, repoURL, repoName, branch, outputFile, clocfile):
-        self.myEmail= myEmail
-        self.myPassword = myPassword
-        self.yourEmail = yourEmail
-        self.repoUrl = repoURL
-        self.repoName = repoName
-        self.branch = branch
-        self.outputFile = outputFile
-        self.clocfile = clocfile
+def validateEmail(email):
+    errorMessage = email
+    if not re.match(r"[^@]+@[^@]+\.[^@]+", email):
+        errorMessage = errorMessage+"Email format is invalid\n"
 
-# def main(self):
-myEmail= input("Type your email: ")
-myPassword = getpass.getpass(prompt='Type your password: ', stream=sys.stderr)
-yourEmail = input("Type the email for report: ")
-repoURL = input("Git URL: ")
-repoName = self.getRepository()
-branch = input("Git branch: ")
-logtime = datetime.now().strftime("%Y%m%d%H%M%S")
-outputFile = "CLOC_"+self.getRepository()+"_"+branch+"_"+logtime+".csv"
-clocfile = "cloc-1.64.exe"
-e = emailCLOC(myEmail, yourEmail, myPassword, repoURL, repoName, branch, outputFile, clocfile)
-e.validateInput(myEmail, yourEmail, myPassword, repoURL, branch, outputFile)
-startTime = datetime.now()
-e.gitinit()
-e.sendEmail()
-endTime = datetime.now()	
-totalTime = endTime - startTime
-print ('Process for branch ' + e.branch + ' of repository ' + e.getRepository() + ' completed successfully (' + str(totalTime.total_seconds()) + ' seconds)')
+def validateURL(url):
+    errorMessage = url
+    if not re.match(r"((git|ssh|http(s)?)|(git@[\w\.]+))(:(//)?)([\w\.@\:/\-~]+)(\.git)(\/)?", url) and (not len(url)==0):
+        errorMessage = errorMessage +"Git repository is invalid.\n"
+
+def setup():
+    speed = [
+        "retrorock8821@gmail.com", 
+        myDefaultPassword.decode(), 
+        "mw103110@gmail.com",
+        "https://github.com/classic89/Forbidden-Island.git", 
+        "master"
+    ]
+    print("Press enter at prompt for speed or enter in your own")
+    # me, pw, yo, ru, br = ().split()
+    me = input("Type your email: ")
+    pw = getpass.getpass(prompt='Type your password: ', stream=sys.stderr)
+    # print("Type your password: ", pw)
+    yo = input("Type the email for report: ")
+    ru = input("Git URL: ")
+    br = input("Git branch: ")
+    count = 0
+    t = [me, pw, yo, ru, br]
+    for x in t:
+        if len(x)==0:
+            t[count] = speed[count]
+        count += 1
+    repoName = getRepository(t[3])
+    logtime = datetime.now().strftime("%Y%m%d%H%M%S")
+    clocfile = "cloc-1.64.exe"
+    outputFile = "CLOC_"+repoName+"_"+t[4]+"_"+logtime+".csv"
+
+    return emailCLOC(t[0], t[1], t[2], t[3], repoName, t[4], outputFile, clocfile)
 
 
 #main
-# if __name__ == "__main__":
-#     main()
+def main():
+    e = setup()
+    e.validateInput()
+    startTime = datetime.now()
+    e.gitinit()
+    e.sendEmail()
+    endTime = datetime.now()
+    totalTime = endTime - startTime
+    print ('Email of the CLOC report on ' + e.branch + ' of ' + e.repoName + ' completed successfully (' + str(totalTime.total_seconds()) + ' seconds)')
+
+if __name__ == "__main__":
+    main()
